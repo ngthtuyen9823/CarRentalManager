ff1764388624de0f787e37cf46de8293db3fcea1
Ngọc Ánh <huynhthingocanh050303@gmail.com>
2023-03-03 12:25:38 +0700

    CRM-02 |Demo Form Main Display
    Logo(Header), Mini/Maximize/Close Form, Form to Fill ( Menu 2 options : no driver and driver, datepicke, ...)

---
+++ FMain/CarRentalManager/customcontrols/MenuBottons.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CarRentalManager.customcontrols
+{
+    /// <summary>
+    /// Interaction logic for MenuBottons.xaml
+    /// </summary>
+    public partial class MenuBottons : UserControl
+    {
+        public MenuBottons()
+        {
+            InitializeComponent();
+        }
+
+
+        public PathGeometry Icon
+        {
+            get { return (PathGeometry)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Icon.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(PathGeometry), typeof(MenuBottons));
+
+
+
+
+        public int IconWidth
+        {
+            get { return (int)GetValue(IconWidthProperty); }
+            set { SetValue(IconWidthProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IconWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconWidthProperty =
+            DependencyProperty.Register("IconWidth", typeof(int), typeof(MenuBottons));
+
+
+
+        public SolidColorBrush IndicatorBrush
+        {
+            get { return (SolidColorBrush)GetValue(IndicatorBrushProperty); }
+            set { SetValue(IndicatorBrushProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IndicatorBrush.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IndicatorBrushProperty =
+            DependencyProperty.Register("IndicatorBrush", typeof(SolidColorBrush), typeof(MenuBottons));
+
+
+
+        public int IndicatorIndicatorCornerRadius
+        {
+            get { return (int)GetValue(IndicatorIndicatorCornerRadiusProperty); }
+            set { SetValue(IndicatorIndicatorCornerRadiusProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IndicatorIndicatorCornerRadius.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IndicatorIndicatorCornerRadiusProperty =
+            DependencyProperty.Register("IndicatorIndicatorCornerRadius", typeof(int), typeof(MenuBottons));
+
+
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(MenuBottons));
+
+
+
+
+        public new Thickness Padding
+        {
+            get { return (Thickness)GetValue(PaddingProperty); }
+            set { SetValue(PaddingProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Padding.  This enables animation, styling, binding, etc...
+        public static new readonly DependencyProperty PaddingProperty =
+            DependencyProperty.Register("Padding", typeof(Thickness), typeof(MenuBottons));
+
+
+
+        public bool IsSelected
+        {
+            get { return (bool)GetValue(IsSelectedProperty); }
+            set { SetValue(IsSelectedProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsSelected.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsSelectedProperty =
+            DependencyProperty.Register("IsSelected", typeof(bool), typeof(MenuBottons));
+
+
+
+        public string GroupName
+        {
+            get { return (string)GetValue(GroupNameProperty); }
+            set { SetValue(GroupNameProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for GroupName.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty GroupNameProperty =
+            DependencyProperty.Register("GroupName", typeof(string), typeof(MenuBottons));
+
+
+
+
+    }
+}
