a4c323aef70ac11e030f0c6d9af6dd387f2193ab
Thanh Tien <phungthanhtien01@gmail.com>
2023-03-04 01:34:00 +0700

    CRM-03 | Add code base for backend

---
+++ src/dao/User.dao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarRentalManager.modals;
+using CarRentalManager.services;
+
+namespace CarRentalManager.dao
+{
+    public class UserDao
+    {
+        public UserDao() { }
+
+        public static User getUserWithEmail(string email)
+        {
+            string queryString = SqlQueryService.getUserWithEmail(email);
+            //*TODO: Update call database later
+            return null;
+        } 
+    }
+}
---
+++ src/modals/Car.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalManager.modals
+{
+    public class Car
+    {
+        string id;
+        string name;
+        string branch;
+        string model;
+        int year;
+        string color;
+        string price;
+        ECarStatus status;
+        ECarType type
+        DateTime createdAt;
+        DateTime updatedAt;
+        public Car(string id;
+        string name,
+            string branch,
+            string model,
+            int year,
+            string color,
+            string price,
+            ECarStatus status,
+            ECarType type,
+            DateTime createdAt,
+            DateTime updatedAt) {
+                this.id = id;
+                this.name = name;
+                this.branch = branch;
+                this.model = model;
+                this.year = year;
+                this.color = color;
+                this.role = role;
+                this.price = price;
+                this.status = status;
+                this.type = type;
+                this.createdAt = new DateTime();
+                this.updatedAt = new DateTime();
+
+    }
+}
