81a9a8670348f82f42f20c900bd1a7136de3ecc9
Thanh Tien <phungthanhtien01@gmail.com>
2023-03-06 01:41:14 +0700

    CRM-04 | Restructure project

---
+++ src/CarRentalManager/dao/RentalInformation.dao.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarRentalManager.modals;
+using CarRentalManager.services;
+
+namespace CarRentalManager.dao
+{
+    public class RentalInformationDAO
+    {
+        public RentalInformationDAO() { }
+
+        public static RentalInformation[] getListRecords()
+        {
+            string queryString = SqlQueryService.getListRecords();
+            //*TODO: Update call database later
+            return null;
+        }
+
+        public static void updateRecord(string id, RentalInformation updatedRecord)
+        {
+            string queryString = SqlQueryService.updateRecord(id, updatedRecord);
+            //*TODO: Update call database later
+        }
+
+        public static void deleteRecord(string id)
+        {
+            string queryString = SqlQueryService.deleteRecord(id);
+            //*TODO: Update call database later
+        }
+    }
+}
---
+++ src/CarRentalManager/modals/RentalInformation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarRentalManager.enums;
+
+namespace CarRentalManager.modals
+{
+    public class RentalInformation
+    {
+        string id;
+        string carId;
+        string userId;
+        DateTime startDate;
+        DateTime endDate;
+        string totalFee;
+        ERentalInformationStatus status;
+        DateTime createdAt;
+        DateTime updatedAt;
+
+        public RentalInformation(string id,
+        string carId,
+        string userId,
+        DateTime startDate,
+        DateTime endDate,
+        string totalFee,
+        ERentalInformationStatus status,
+        DateTime createdAt,
+        DateTime updatedAt)
+        {
+            this.id = id;
+            this.carId = carId;
+            this.userId = userId;
+            this.startDate = startDate;
+            this.endDate = endDate;
+            this.totalFee = totalFee;
+            this.status = status;
+            this.createdAt = createdAt;
+            this.updatedAt = updatedAt;
+        }
+
+        public string ID { get { return id; } }
+        public string CarId { get { return carId; } }
+        public string UserId { get { return userId; } }
+        public DateTime StartDate { get { return startDate; } }
+        public DateTime EndDate { get { return endDate; } }
+        public string TotalFee { get { return totalFee; } }
+        public ERentalInformationStatus Status { get { return status; } }
+        public DateTime CreatedAt { get { return createdAt; } }
+        public DateTime UpdatedAt { get { return updatedAt; } }
+
+
+    }
+}
