3c653ec616728ebe0b9313d89af8d11cf4403fb0
Thanh Tien <phungthanhtien01@gmail.com>
2023-03-07 23:55:29 +0700

    Add Car, Diagram

---
+++ src/CarRentalManager/enums/EDrivingType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalManager.enums
+{
+   public enum EDrivingType
+    {
+        SELF_DRIVING,
+        MANNED
+    }
+}
