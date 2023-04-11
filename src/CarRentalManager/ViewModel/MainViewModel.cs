72b2d703b05bf532f55385103189d7a56ff144b6
Nguyen Thi Thanh Tuyen <camuccon98@gmail.com>
2023-04-11 22:09:48 +0700

    CRM-37 | fixed default view

--- src/CarRentalManager/ViewModel/MainViewModel.cs
+++ src/CarRentalManager/ViewModel/MainViewModel.cs
@@ -12,7 +12,6 @@ namespace CarRentalManager.ViewModel
     public class MainViewModel : BaseViewModel
     {
         public bool isLoaded = false;
-        public ICommand LoadedWindowCommand { get; set; }
         public ICommand CustomerCommand { get; set; }
         public ICommand SupplierCommand { get; set; }
         public ICommand ContractCommand { get; set; }
@@ -21,16 +20,17 @@ namespace CarRentalManager.ViewModel
         public ICommand RegisterFormCommand { get; set; }
         public ICommand HomeCommand { get; set; }
 
-        public BaseViewModel CurrentChildView { get { return currentChildView; } set { currentChildView = value; OnPropertyChanged(nameof(currentChildView)); } }
         public string CapTion { get { return capTion; } set { capTion = value; OnPropertyChanged(nameof(capTion)); } }
         private BaseViewModel currentChildView;
         private string capTion;
+        public BaseViewModel CurrentChildView { get { return currentChildView; } set { currentChildView = value; OnPropertyChanged(nameof(currentChildView)); } }
         public MainViewModel()
         {
-            LoadedWindowCommand = new RelayCommand<Window>((p) => { return true; }, (p) =>
-            {
-                p.Show();
-            });
+            ExecuteShowHomeViewCommand(null);
+            SetCommands();
+        }
+        private void SetCommands()
+        {
             CarCommand = new RelayCommand<object>((p) => { return true; }, (p) => { CurrentChildView = new ListCarViewModel(); });
             CustomerCommand = new RelayCommand<object>((p) => { return true; }, (p) => { CurrentChildView = new ListCustomerViewModel(); });
             ContractCommand = new RelayCommand<object>((p) => { return true; }, (p) => { CurrentChildView = new ListContractViewModel(); });
