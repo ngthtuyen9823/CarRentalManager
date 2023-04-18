2c55aa5db4d829081956160f4fc1b225c130d648
Thanh Tien <phungthanhtien01@gmail.com>
2023-04-18 15:53:41 +0700

    CRM-38 | Handle login

---
+++ src/CarRentalManager/state/LoginInInforState.cs
@@ -0,0 +1,16 @@
+﻿using CarRentalManager.enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalManager.state
+{
+    public static class LoginInInforState
+    {
+        public static int ID { get; set; }
+        public static string Name { get; set; }
+        public static EUserRole Role { get; set; }
+    }
+}
