1cf79c48d45acf5608b65acaa6d66d1e797ead23
Nguyen Thi Thanh Tuyen <camuccon98@gmail.com>
2023-04-26 23:31:25 +0700

    CRM-41 | handle function render chart

--- src/CarRentalManager/StatisticView.xaml.cs
+++ src/CarRentalManager/StatisticView.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using LiveCharts.Wpf.Charts.Base;
 using CarRentalManager.services;
+using System.Security.AccessControl;
 
 namespace CarRentalManager
 {
@@ -25,37 +26,73 @@ namespace CarRentalManager
     public partial class StatisticView : UserControl
     {
         readonly StatisticsService statisticsService = new StatisticsService();
+        public Func<ChartPoint, string> PointLabel { get; set; }
+        public ChartValues<double> Values { get; set; }
+
         public StatisticView()
         {
             InitializeComponent();
+
             PointLabel = chartPoint => string.Format("{0} ({1:P})", chartPoint.Y, chartPoint.Participation);
+            drawChartByOnrentTimes();
+            drawChartByBrokenTimes();
+
             Values = new ChartValues<double>();
+            drawChartByYear();
+            //drawChartByMonth();
+            //drawChartByPreciouse();
 
-            //Dictionary<string, int> dictYear = statisticsService.getDictTotalRevenueByYear();
-            //for (int i = 2019; i <= 2023; i++)
-            //{
-            //    double x = dictYear[$"{i}"] / (double)1000;
-            //    Values.Add(x);
-            //}
-
-
+            DataContext = this;
+        }
+        private void drawChartByBrokenTimes()
+        {
+            Dictionary<string, int> dictBrokenTimes = statisticsService.getDictBrokennTimes();
+            drawPieChart(dictBrokenTimes);
+        }
+        private void drawChartByOnrentTimes()
+        {
+            Dictionary<string, int> dictOnrentTimes = statisticsService.getDictOnrentTimes();
+            drawPieChart(dictOnrentTimes);
+        }
+        private void drawPieChart(Dictionary<string, int> dict)
+        {
+            var sortedDict = from entry in dict orderby entry.Value descending select entry;
+            Piece1.Title = sortedDict.ElementAt(0).Key;
+            Piece1.Values = new ChartValues<double> { sortedDict.ElementAt(0).Value };
+            Piece2.Title = sortedDict.ElementAt(1).Key;
+            Piece2.Values = new ChartValues<double> { sortedDict.ElementAt(1).Value };
+            Piece3.Title = sortedDict.ElementAt(2).Key;
+            Piece3.Values = new ChartValues<double> { sortedDict.ElementAt(2).Value };
+            Piece4.Title = sortedDict.ElementAt(3).Key;
+            Piece4.Values = new ChartValues<double> { sortedDict.ElementAt(3).Value };
+        }
+        private void drawChartByPreciouse()
+        {
+            Dictionary<string, int> dictPreciouse = statisticsService.getDictTotalRevenueByPrecious();
+            for (int i = 1; i <= 4; i++)
+            {
+                double x = dictPreciouse[$"{i}"] / (double)10;
+                Values.Add(x);
+            }
+        }
+        private void drawChartByMonth()
+        {
             Dictionary<string, int> dictMonth = statisticsService.getDictTotalRevenueByMonth();
             for (int i = 1; i <= 12; i++)
             {
                 double x = dictMonth[$"{i}"] / (double)10;
                 Values.Add(x);
             }
-
-            //Dictionary<string, int> dictPreciouse = statisticsService.getDictTotalRevenueByPrecious();
-            //for (int i = 1; i <= 4; i++)
-            //{
-            //    double x = dictPreciouse[$"{i}"];
-            //    Values.Add(x);
-            //}
-            DataContext = this;
         }
-        public Func<ChartPoint, string> PointLabel { get; set; }
-
+        private void drawChartByYear()
+        {
+           Dictionary<string, int> dictYear = statisticsService.getDictTotalRevenueByYear();
+            for (int i = 2019; i <= 2023; i++)
+            {
+                double x = dictYear[$"{i}"] / (double)1000;
+                Values.Add(x);
+            }
+        }
         private void Chart_OnDataClick(object sender, ChartPoint chartpoint)
         {
             var chart = (LiveCharts.Wpf.PieChart)chartpoint.ChartView;
@@ -67,69 +104,9 @@ namespace CarRentalManager
             var selectedSeries = (PieSeries)chartpoint.SeriesView;
             selectedSeries.PushOut = 8;
         }
-        public ChartValues<double> Values { get; set; }
-
         private void UpdateOnclick(object sender, RoutedEventArgs e)
         {
             Chart.Update(true);
         }
-        private void showData(object sender, RoutedEventArgs e)
-        {
-            // Vo ham nay coi cach lay list ra
-            Dictionary<string, int> dictOnrentTimes = statisticsService.getDictOnrentTimes();
-            Dictionary<string, int> dictBrokenTimes = statisticsService.getDictBrokennTimes();
-
-            foreach (var dict in dictBrokenTimes)
-            {
-                MessageBox.Show($"Key: {dict.Key}, Value: {dict.Value}");
-            }
-            /*
-            foreach (var dict in dictOnrentTimes)
-            {
-                object carId;
-                bool hasValueId = dict.TryGetValue("carId", out carId);
-                object brokenTimes;
-                bool hasValueOnerent = dict.TryGetValue("brokenTimes", out brokenTimes);
-                MessageBox.Show(brokenTimes.ToString());
-            }*/
-            //Piece1.Title = dictOnrentTimes
-            //Piece1.Values = new ChartValues<double> { 10 };
-            
-            //foreach (var dict in dictOnrentTimes)
-            //{
-            //    object carId;
-            //    bool hasValueId = dict.TryGetValue("carId", out carId);
-            //    object brokenTimes;
-            //    bool hasValueOnerent = dict.TryGetValue("brokenTimes", out brokenTimes);
-            //    MessageBox.Show(brokenTimes.ToString());
-            //}
-
-            Dictionary<string, int> dictYear =  statisticsService.getDictTotalRevenueByYear();
-            Dictionary<string, int> dictMonth =  statisticsService.getDictTotalRevenueByMonth();
-            Dictionary<string, int> dictPreciouse =  statisticsService.getDictTotalRevenueByPrecious();
-            
-            //YEAR
-
-            for (int i = 2019; i <= 2023; i++)
-            {
-                //MessageBox.Show(dictYear[$"{i}"].ToString());
-                Values.Add(dictYear[$"{i}"]);
-            }
-
-            //MONTH
-            /*for (int i = 1; i <= 12; i++)
-            {
-                MessageBox.Show(dictMonth[$"{i}"].ToString());
-            }*/
-
-            //PRECIOUSE
-            /*for (int i = 1; i <= 4; i++)
-            {
-                MessageBox.Show(dictPreciouse[$"{i}"].ToString());
-            }*/
-        }
-    
-    
-    
     }
 }
