e827d449d5227c6cfca84bd6f27f92c23e5a315b
Ngọc Ánh <huynhthingocanh050303@gmail.com>
2023-05-27 16:47:33 +0700

    CRME - 05 | SupplierDAO + CustomerDAO

--- src/CarRentalManager/dao/SupplierDAO.cs
+++ src/CarRentalManager/dao/SupplierDAO.cs
@@ -5,43 +5,39 @@ using System.Data;
 using CarRentalManager.models;
 using System.Windows;
 using System.Linq;
+using System.Data.Entity.Migrations;
 
 namespace CarRentalManager.dao
 {
     public class SupplierDAO
     {
-        readonly SqlQueryService sqlService = new SqlQueryService();
-        readonly CommondDataService commondDataService = new CommondDataService();
-        readonly DbConnectionDAO dbConnectionDAO = new DbConnectionDAO();
+        readonly Context db =  new Context();
         public SupplierDAO() { }
 
         public List<Supplier> getListSupplier()
         {
-            string sqlStringGetTable = sqlService.getListTableData(ETableName.SUPPLIER);
-            DataTable dataTable = dbConnectionDAO.getDataTable(sqlStringGetTable);
-            return commondDataService.dataTableToList<Supplier>(dataTable);
+            return db.Suppliers.ToList();
         }
 
         public void createSupplier(Supplier supplier)
         {
-            string sqlString = sqlService.createSupplier(supplier);
-            dbConnectionDAO.getDataTable(sqlString);
+            db.Suppliers.Add(supplier);
+            db.SaveChanges();
         }
         public void removeSupplier(int id)
         {
-            string sqlString = sqlService.removeById(ETableName.SUPPLIER, id);
-            dbConnectionDAO.getDataTable(sqlString);
+            var supplier = db.Suppliers.FirstOrDefault(s => s.id == id);
+            db.Suppliers.Remove(supplier);
+            db.SaveChanges();
         }
         public void updatSupplier(Supplier supplier)
         {
-            string sqlString = sqlService.updateSupplier(supplier);
-            dbConnectionDAO.getDataTable(sqlString);
+            db.Suppliers.AddOrUpdate(supplier);
+            db.SaveChanges();
         }
         public Supplier getInforByEmail(string email)
         {
-            string sqlStringGetTable = sqlService.getCreadentialWithEmail(ETableName.SUPPLIER, email);
-            DataTable dataTable = dbConnectionDAO.getDataTable(sqlStringGetTable);
-            return commondDataService.dataTableToList<Supplier>(dataTable)?.First();
+            return db.Suppliers.FirstOrDefault(supplier => supplier.email == email);
         }
     }
 }
